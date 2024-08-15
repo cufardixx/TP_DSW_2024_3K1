c6a63de5721480f62d3d99640aef54f94e224ee4
Tomas Yasparra <tomasyas@gmail.com>
2024-08-15 19:09:59 -0300

    update .gitignore

--- Backend/dist/category/category.entity.js
+++
@@ -1,9 +0,0 @@
-import crypto from "node:crypto";
-export class Category {
-    constructor(name, description, id = crypto.randomUUID()) {
-        this.name = name;
-        this.description = description;
-        this.id = id;
-    }
-}
-//# sourceMappingURL=category.entity.js.map

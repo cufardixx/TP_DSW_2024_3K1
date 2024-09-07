4a554cc47ba177e42ac084b995fe79f77adcd327
cufardixx <facundopicia@gmail.com>
2024-09-07 16:07:21 -0300

    componente register parte visual

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
-    { path: "", component: LoginComponent },
+    { path: "", component: LandingComponent },
     { path: "**", redirectTo: "", pathMatch: "full" }
 ];

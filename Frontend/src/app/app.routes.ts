fbeada257b0bf56301298bf036b41440562ed4cd
cufardixx <facundopicia@gmail.com>
2024-09-07 14:57:08 -0300

    componente login parte visual

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-    { path: "", component: LandingComponent },
+    { path: "", component: LoginComponent },
     { path: "**", redirectTo: "", pathMatch: "full" }
 ];

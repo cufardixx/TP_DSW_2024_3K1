9c793f2daa1445c67ca6506bc69bab3b64d72982
cufardixx <facundopicia@gmail.com>
2024-09-08 16:28:14 -0300

    spa

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
-    { path: "", component: LandingComponent },
+    { path: "", component: LandingComponent , title: 'Home'},
+    { path: "login", component: LoginComponent ,title: 'Login'},
+    { path: "register", component: RegisterComponent, title: 'Register' },
     { path: "**", redirectTo: "", pathMatch: "full" }
 ];

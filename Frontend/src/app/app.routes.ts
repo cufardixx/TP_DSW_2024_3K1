6449909114c010fa83dac5329b588e676194068f
cufardixx <facundopicia@gmail.com>
2024-09-17 20:02:04 -0300

    componennte profile

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
 
 export const routes: Routes = [
     { path: "", component: LandingComponent , title: 'Home'},
     { path: "login", component: LoginComponent ,title: 'Login'},
     { path: "register", component: RegisterComponent, title: 'Register' },
+    { path: "profile", component: PerfilComponent, title: 'Perfil' },
     { path: "**", redirectTo: "", pathMatch: "full" }
 ];

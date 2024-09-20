3ad9d1fb722543cdb86a528a8919d6584f5893d4
cufardixx <facundopicia@gmail.com>
2024-09-20 10:12:49 -0300

    logica evento sin terminar

--- Frontend/src/app/pages/registrar-evento/registrar-evento.component.ts
+++ Frontend/src/app/pages/registrar-evento/registrar-evento.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { EventServiceService } from '../../services/event.service.service';
 
 @Component({
   selector: 'app-registrar-evento',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registrar-evento.component.html',
   styleUrl: './registrar-evento.component.css'
 })
 export class RegistrarEventoComponent {
+  private AccesService = inject(EventServiceService);
+  private router = inject(Router);
+  public formBuild = inject(FormBuilder);
 
+  public formRegistro: FormGroup = this.formBuild.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    date: ['', Validators.required],
+    time: ['', Validators.required],
+    location: ['', Validators.required],
+    image: ['', Validators.required],
+    price: ['', Validators.required],
+  })
+
+
+  
 }
